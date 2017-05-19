4612e97156368da4dd17bab60827d787c01922d7
Admin <charleeagni12@gmail.com>
2017-05-19 20:16:46 +0530

    zombie created or 7(not sure :P)

---
+++ 7_create_zombie.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+
+
+
+int main(int argc, char const *argv[]) {
+  int pr;
+  if((pr = fork()) <0){
+    std::cout << "error while forking" << '\n';
+    return -1;
+  }
+  else if(pr > 0){
+    std::cout << "child" << '\n';
+    sleep(2);
+    system("ps -o pid,ppid,tty,state,command");
+  }
+  else{
+    std::cout << "parent created" << '\n';
+    _exit(0);
+  }
+
+  return 0;
+}
